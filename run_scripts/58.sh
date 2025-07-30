da051d123c78b779753ef9faaff33b0e5b0eeb91
Ubuntu <ubuntu@ip-172-31-2-82.us-west-2.compute.internal>
2025-07-30 18:19:47 +0000

    gemma trainer

--- run_scripts/58.sh
+++ run_scripts/58.sh
@@ -1,9 +1,19 @@
-CUDA_VISIBLE_DEVICES=7,6,5,4,3,2,1,0 python grpo_discounted.py \
-    --model_name "Qwen/Qwen2.5-1.5B-Instruct" \
+# python grpo_discounted.py \
+#     --model_name "google/gemma-2b-it" \
+#     --gamma 1.0 \
+#     --seed 42 \
+#     --machine_name "constantlr-1syncsteps-1epoch-58"\
+#     --sync_ref_model \
+#     --disable_dropout \
+#     --ref_model_sync_steps 1 \
+#     --beta 0.01 
+
+python gemma_grpo.py \
+    --model_name "google/gemma-2b-it" \
     --gamma 1.0 \
-    --seed 43 \
+    --seed 42 \
     --machine_name "constantlr-1syncsteps-1epoch-58"\
     --sync_ref_model \
     --disable_dropout \
     --ref_model_sync_steps 1 \
-
+    --beta 0.01 
